6f74436ff56da79fcceff9da27f48897434152fd
juanppf <juanp972tri@gmail.com>
2017-03-17 20:20:22 +0000

    Initial commit

---
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.Drop;
+import com.mygdx.game.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
